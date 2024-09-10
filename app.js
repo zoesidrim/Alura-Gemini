b08b3d668097c374e8839d4d1478cc163249836f
Zoé Sidrim <163189189+zoesidrim@users.noreply.github.com>
2024-09-10 17:53:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    let resultados = "";   
+    let titulo = "";
+    let descricao = "";
+
+    
+    if (campoPesquisa == "" || campoPesquisa ==" "){
+        section.innerHTML= "Nenhum cantor foi encontrado";
+        return;
+    }
+    
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    for (let dado of dados) {
+        
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+            `
+       } 
+
+       if (!resultados){
+        resultados = "Nenhum cantor foi encontrado"
+       }
+    }
+        
+    section.innerHTML = resultados;
+        
+}
+
